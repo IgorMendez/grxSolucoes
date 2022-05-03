d4d87bda92bf9424bd8b805123a435cb9c3dff2c
IgorMendez <igor.emp@hotmail.com>
2022-05-03 03:16:22 -0300

    feat: Criação do Header

---
+++ front-end/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from '../images/logo.png';
+
+export default function Header() {
+  return (
+    <header>
+      <div />
+      <img src={Logo} alt="Logotipo principal GRX" />
+    </header>
+  );
+}
