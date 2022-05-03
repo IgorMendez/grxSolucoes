e4f5c4044ffdc03621a70a0617ae1162b53b07ef
IgorMendez <igor.emp@hotmail.com>
2022-05-03 03:18:48 -0300

    feat: Criação da pagina inicial

---
+++ front-end/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/header';
+import FormContainer from '../components/formContainer';
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+      <FormContainer />
+    </div>
+  );
+}
