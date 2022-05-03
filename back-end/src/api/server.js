bbe630c5f2499bac58bc72d01fe3eeb2c47e4959
IgorMendez <igor.emp@hotmail.com>
2022-05-03 05:12:52 -0300

    feat: atualização do build do front

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -5,10 +5,13 @@ const app = express();
 
 const datesRoute = require('../database/routes/datas');
 
+app.use(cors());
 app.use(express.json());
-
+app.use('/', express.static(`${__dirname}/build`));
 app.use('/data', datesRoute);
 
-app.use(cors());
+app.get('/avaliacao-online', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
+
+app.get('/avaliacao-cautelar', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
 module.exports = app;
